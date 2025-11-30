dea422a605b7d7bbad596e69054ed01b639d6cd0
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-11-30 12:19:09 +0530

    feat(new learnings): Added new learning in zod

--- Zod/zod-learning/src/App.tsx
+++ Zod/zod-learning/src/App.tsx
@@ -4,15 +4,17 @@ import { z } from 'zod'
 function App() {
 
 const UserSchema = z.object({
-    username: z.string(),
-    
+  username: z.string(),
+  age: z.number(),
+  address: z.string().optional(),
+  isProgrammer: z.boolean()
 })
 
 type User = z.infer<typeof UserSchema>
 
-const user = { username: "Anu" }
+const user = { username: "Anu", age: 2, address: "Puthan purayil", isProgrammer: true }
 
-console.log(UserSchema.safeParse(user))
+  console.log(UserSchema.safeParse(user).success)
 
   return (
     <>
